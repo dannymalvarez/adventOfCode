d279b74d0bdafec43ae103aea7b62e3268e6e584
Daniel Alvarez <92490029+dannymalvarez@users.noreply.github.com>
2022-04-08 14:39:32 -0400

    Update with cleaner code
    This is the cleaner version of code for 2021 advent of code day 1 part 1

--- AdventOfCode/Day_1/puzzleCounter.py
+++ AdventOfCode/Day_1/puzzleCounter.py
@@ -1,25 +1,14 @@
-data = []
-inputs = open('input.txt','r')
-datum = inputs.read()
-counter = 0 
-datumb = datum.split('\n')
-data.append(datumb)
-data = data.pop()
-
-data.remove('')
-   
-for i in range(0, len(data)):
-  data[i] = int(data[i])
-
+data = open("input.txt", "r")
+printData = data.read()
+x = printData.split()
 counter = 0
-for i in range(0, len(data)):
-  a = data[i]
-  b = data[i+1]
+length = len(x)
+lengthEnd = length - 1
+for i in range(0, lengthEnd):
+  a = int(x[i])
+  b = int(x[i+1])
   if b > a:
-    counter += 1
-    print(counter)
-# Answer is 1766, also I get a fun error once it finishes running
-#Traceback (most recent call last):
-#  File "/Users/danielalvarez/Desktop/Fun_Projects/AdventOfCode/Day_1/puzzleCounter.py", line 17, in <module>
-#    b = data[i+1]
-#IndexError: list index out of range
+      counter = counter + 1
+print(counter)
+# Answer is 1766
+#This is a cleaner version of the solution and done in python which was my preferred language.
