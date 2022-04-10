decfe7db4a789ea6ac83fd7ad632e25e7e5a35bb
Daniel Alvarez <92490029+dannymalvarez@users.noreply.github.com>
2022-04-10 14:24:21 -0400

    Final answer for day1 part 2

--- AdventOfCode/Day_1/puzzleCounterPT2.py
+++ AdventOfCode/Day_1/puzzleCounterPT2.py
@@ -1,17 +1,13 @@
-#Work in Progress on repl.it
-data = open("input2.txt", "r")
+data = open("input.txt", "r")
 printData = data.read()
 x = printData.split()
 counter = 0
 length = len(x)
-lengthEnd = length - 1
+lengthEnd = length - 3
 for i in range(0, lengthEnd):
-  if x[i] and x[i+1] and x[i+2]:
-    sum = int(x[i]) + int(x[i+1]) + int(x[i+2])
-    print(sum)
-  #a = int(x[i])
-  #b = int(x[i+1])
-  #sum = a + b + c
-  #if b > a:
-   #   counter = counter + 1
-#print(counter)
+  a = int(x[i]) 
+  b = int(x[i+3])
+  if b > a:
+    counter = counter + 1
+print(counter)
+# answer is 1797
